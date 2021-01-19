2a821f7d8c03752d69a6c216ac399074daf1c03e
Andrea Walchshofer <flaxline@eclipso.at>
2021-01-19 21:42:11 +0100

    Add JavaDoc Comments

--- src/test/java/at/fhhagenberg/sqe/pageobject/AppPO.java
+++ src/test/java/at/fhhagenberg/sqe/pageobject/AppPO.java
@@ -20,83 +20,148 @@ public class AppPO {
 	
 	/* IDs of UI Elements */
 	
+	/**
+	 * @return CSS id of Current Floor
+	 */
 	public String GetCurrentFloorLabel() {
 		return "#lbFloor";
 	}
 	
+	/**
+	 * @return CSS id of Down Image
+	 */
 	public String GetDownImage() {
 		return "#imgElevDown";
 	}
 	
+	/**
+	 * @return CSS id of Up Image
+	 */
 	public String GetUpImage() {
 		return "#imgElevUp";
 	}
 	
+	/**
+	 * @return CSS id of Payload
+	 */
 	public String GetPayloadLabel() {
 		return "#lbPayload";
 	}
 	
+	/**
+	 * @return CSS id of Speed
+	 */
 	public String GetSpeedLabel() {
 		return "#lbSpeed";
 	}
 	
+	/**
+	 * @return CSS id of Door Status
+	 */
 	public String GetDoorsLabel() {
 		return "#lbDoors";
 	}
 	
+	/**
+	 * @return CSS id of next Target
+	 */
 	public String GetTargetLabel() {
 		return "#lbTarget";
 	}
 	
+	/**
+	 * @return CSS id of Error ListView
+	 */
 	public String GetErrorListView() {
 		return "#lvErrors"; 
 	}
 	
+	/**
+	 * @param robot - FxRobot of test
+	 * @return First Object in Error LV
+	 */
 	public Object GetFirstError(FxRobot robot) {
 		return robot.lookup(GetErrorListView()).queryAs(ListView.class).getItems().get(0);
 	}
 	
+	/**
+	 * @return CSS id of Floors ListView
+	 */
 	public String GetFloorListView() {
 		return "#lvFloors"; 
 	}
 	
+	/**
+	 * @return CSS id of Manual Mode RB
+	 */
 	public String GetManualRB() {
 		return "#rbManual";
 	}
 	
+	/**
+	 * @return CSS id of Automatic Mode RB
+	 */
 	public String GetAutomaticRB() {
 		return "#rbAuto";
 	}
 
+	/**
+	 * @return CSS id of Floor Number
+	 */
 	public String GetFloorNumberLabel() {
 		return "#floorNumber";
 	}
 	
+	/**
+	 * @return CSS id of Elevator Combobox
+	 */
 	public String GetElevatorCombobox() {
 		return "#cmbElevators";
 	}
+	
+	
 
 	/* Actions */
 	
+	/**
+	 * Click on a "set target" button
+	 * @param robot - FxRobot of test
+	 */
 	public void ClickSetTarget(FxRobot robot) {
 		// just take first button
 		robot.clickOn(".button");
 	}
 	
+	
+	
 	/* Assertions */
 	
+	/**
+	 * @param actual - Object to do assertion on
+	 * @param expected - expected value
+	 * @throws InterruptedException
+	 */
 	public void VerifyEquals(Object actual, Object expected) throws InterruptedException {
 		assertAfterJavaFxPlatformEventsAreDone(() -> {
 			assertEquals(expected, actual);
 		});
 	}
 	
+	/**
+	 * @param elementId - CSS id of element
+	 * @param value - expected value
+	 * @throws InterruptedException
+	 */
 	public void VerifyLabel(String elementId, String value) throws InterruptedException {
 		assertAfterJavaFxPlatformEventsAreDone(() -> {
 			FxAssert.verifyThat(elementId, LabeledMatchers.hasText(value));
 		});
 	}
 	
+	/**
+	 * @param pairs - Pairs of CSS id of element and expected value
+	 * @throws InterruptedException
+	 */
 	public void VerifyLabels(Pair<String, String>... pairs) throws InterruptedException {
 		assertAfterJavaFxPlatformEventsAreDone(() -> {
 			for(var pair : pairs) {
